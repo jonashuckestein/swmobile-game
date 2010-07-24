52b664a8f70e6fd037f7be352f1c0756107972d3
Peter Dolan <peterjdolan@Peter-Dolans-MacBook-Pro.local>
2010-07-24 13:34:42 -0700

    some work on the player controller

--- controller/player.py
+++ controller/player.py
@@ -1,1 +1,11 @@
 """This is the player's controller."""
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+
+
+class PlayerHandler(webapp.RequestHandler):
+
+  def get(self):
+    user = users.get_current_user()
+    
--- model/player.py
+++ model/player.py
@@ -1,1 +1,16 @@
 """This is the player's model"""
+
+from google.appengine.ext import db
+
+class Player(db.Model):
+
+  user = db.UserProperty(required=True)
+
+  location = db.GeoPtProperty(required=False)
+  
+  # Game attributes
+  max_health = db.IntegerProperty(required=True)
+  current_health = db.IntegerProperty(required=True)
+  level = db.IntegerProperty(required=True, default=1)
+  strength = db.IntegerProperty(required=True)
+  dexterity = db.IntegerProperty(required=True)
