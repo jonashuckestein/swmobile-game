56a921df22bf06b82a6fdf1d313ed3ecfa5f645c
Peter Dolan <peterjdolan@Peter-Dolans-MacBook-Pro.local>
2010-07-24 14:02:38 -0700

    some more work

--- controller/player.py
+++ controller/player.py
@@ -2,10 +2,20 @@
 
 from google.appengine.api import users
 from google.appengine.ext import webapp
-
+from model import Player as PlayerModel
+from controller import util
 
 class PlayerHandler(webapp.RequestHandler):
 
   def get(self):
     user = users.get_current_user()
+    player = PlayerModel.get_or_insert(user.user_id(), user=user)
+    util.Output(self._AsJsonDict(player))
     
+  def _AsJsonDict(self, player):
+    d = {"email": player.user.email()}
+    if player.location is not None:
+      d["lat"] = player.location.lat
+      d["lon"] = player.location.lon
+    if player.max_health is not None and player.current_health is not None:
+      
--- model/player.py
+++ model/player.py
@@ -6,11 +6,11 @@ class Player(db.Model):
 
   user = db.UserProperty(required=True)
 
-  location = db.GeoPtProperty(required=False)
+  location = db.GeoPtProperty()
   
   # Game attributes
-  max_health = db.IntegerProperty(required=True)
-  current_health = db.IntegerProperty(required=True)
-  level = db.IntegerProperty(required=True, default=1)
-  strength = db.IntegerProperty(required=True)
-  dexterity = db.IntegerProperty(required=True)
+  max_health = db.IntegerProperty()
+  current_health = db.IntegerProperty()
+  level = db.IntegerProperty()
+  strength = db.IntegerProperty()
+  dexterity = db.IntegerProperty()
