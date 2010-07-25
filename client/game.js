6f3638ff7f79b5d0e4845b9f8ca88323ce6078d9
Jonas Huckestein <jonas.huckestein@me.com>
2010-07-25 11:51:20 -0700

    blub

--- client/game.js
+++ client/game.js
@@ -5,54 +5,7 @@ Ext.setup({
     phoneStartupScreen: 'TODO.png',
     glossOnIcon: true,
     onReady: function() {
-      
-        // utilities
-        var kRadiusOfEarthMeters = 6378100;
 
-        // Takes the position object from the navigator's location services,
-        // returns a google.maps.LatLng
-        function latLngFromPosition(position) {
-          return new google.maps.LatLng(position.coords.latitude,
-                                        position.coords.longitude);
-        }
-
-        // Convert degrees to radians.
-        function toRadians(degrees) {
-          return degrees * Math.PI / 180;
-        }
-
-        // Distance, in meters, between points a and b
-        // a and b are both google.maps.LatLng
-        function distance(a, b) {
-          // Haversine formula, from http://mathforum.org/library/drmath/view/51879.html
-          dLat = a.lat() - b.lat();
-          dLon = a.lng() - b.lng();
-          var x = Math.pow(Math.sin(toRadians(dLat / 2)), 2) +
-              Math.cos(toRadians(a.lat())) *
-                Math.cos(toRadians(b.lat())) *
-                  Math.pow(Math.sin(toRadians(dLon / 2)), 2);
-          var greatCircleDistance = 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1-x));
-          return kRadiusOfEarthMeters * greatCircleDistance;
-        }
-
-        // origin and target are google.maps.LatLng, distanceMeters is numeric
-        function latLngTowardTarget(origin, target, distanceMeters) {
-          dLat = target.lat() - origin.lat();
-          dLon = target.lng() - origin.lng();
-
-          dist = distance(origin, target);
-          offsetLat = dLat * distanceMeters / dist;
-          offsetLng = dLon * distanceMeters / dist;
-          return new google.maps.LatLng(origin.lat() + offsetLat, origin.lng() + offsetLng);
-        }
-
-        // Get a random latitude and longitude near the given point.
-        function randomLatLngNearLocation(origin, distanceMeters) {
-          var randLat = origin.lat() + Math.random() - 0.5;
-          var randLng = origin.lng() + Math.random() - 0.5;
-          var targetLoc = new google.maps.LatLng(randLat, randLng);
-          return latLngTowardTarget(origin, targetLoc, distanceMeters);
-        }
 
       var worldRefreshLoop = function() {
         // TODO show loading icon somewhere
@@ -121,7 +74,7 @@ Ext.setup({
         console.log("updated game state", game.worldState);
       }
       var startQuest = function(target) {
-        mapPanel.map.setZoom(20);
+
         game.quest = new HippieHunt(target, game);
 
 
@@ -134,9 +87,11 @@ Ext.setup({
 
       
       // have a timer updating the gamestate as fast as possible with 1 sec delay after response comes in
+      var questPanelTextField = new Ext.form.TextField({
+        name: 'targetAddress'
+      });
       var questPanel = new Ext.Panel({
-        
-        
+                
         title: 'Quest',
         iconCls: 'info',
         cls: 'quest',
@@ -156,21 +111,22 @@ Ext.setup({
                   required: true,
                   labelAlign: 'left'
               },
-              items: [{
-                  xtype: 'textfield',
-                  name : 'name'
-              }]
+              items: [questPanelTextField]
               },
           
             {
               xtype: 'button',
               text: 'Start quest',
               handler: function(){
+                var address = questPanelTextField.getValue();
+                if(!address) {
+                  alert("Make sure to enter a valid address!");
+                  return;
+                }
                 var geocoder = new google.maps.Geocoder();
                 geocoder.geocode({
-                  address: "1400 Washington St, 94109 San Francisco"
+                  address: address
                 }, function(response){
-                
                   startQuest({
                     lat: response[0].geometry.location.lat(),
                     lon: response[0].geometry.location.lng()
