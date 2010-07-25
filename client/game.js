dfc8a3623fbe35295508b9e6558e322dcb0f0d04
Peter Dolan <peterjdolan@Peter-Dolans-MacBook-Pro.local>
2010-07-25 12:46:14 -0700

    save the user when their location is updated.

--- client/game.js
+++ client/game.js
@@ -5,55 +5,6 @@ Ext.setup({
     phoneStartupScreen: 'TODO.png',
     glossOnIcon: true,
     onReady: function() {
-      
-        // utilities
-        var kRadiusOfEarthMeters = 6378100;
-
-        // Takes the position object from the navigator's location services,
-        // returns a google.maps.LatLng
-        function latLngFromPosition(position) {
-          return new google.maps.LatLng(position.coords.latitude,
-                                        position.coords.longitude);
-        }
-
-        // Convert degrees to radians.
-        function toRadians(degrees) {
-          return degrees * Math.PI / 180;
-        }
-
-        // Distance, in meters, between points a and b
-        // a and b are both google.maps.LatLng
-        function distance(a, b) {
-          // Haversine formula, from http://mathforum.org/library/drmath/view/51879.html
-          dLat = a.lat() - b.lat();
-          dLon = a.lng() - b.lng();
-          var x = Math.pow(Math.sin(toRadians(dLat / 2)), 2) +
-              Math.cos(toRadians(a.lat())) *
-                Math.cos(toRadians(b.lat())) *
-                  Math.pow(Math.sin(toRadians(dLon / 2)), 2);
-          var greatCircleDistance = 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1-x));
-          return kRadiusOfEarthMeters * greatCircleDistance;
-        }
-
-        // origin and target are google.maps.LatLng, distanceMeters is numeric
-        function latLngTowardTarget(origin, target, distanceMeters) {
-          dLat = target.lat() - origin.lat();
-          dLon = target.lng() - origin.lng();
-
-          dist = distance(origin, target);
-          offsetLat = dLat * distanceMeters / dist;
-          offsetLng = dLon * distanceMeters / dist;
-          return new google.maps.LatLng(origin.lat() + offsetLat, origin.lng() + offsetLng);
-        }
-
-        // Get a random latitude and longitude near the given point.
-        function randomLatLngNearLocation(origin, distanceMeters) {
-          var randLat = origin.lat() + Math.random() - 0.5;
-          var randLng = origin.lng() + Math.random() - 0.5;
-          var targetLoc = new google.maps.LatLng(randLat, randLng);
-          return latLngTowardTarget(origin, targetLoc, distanceMeters);
-        }
-
       var worldRefreshLoop = function() {
         // TODO show loading icon somewhere
         Ext.Ajax.request({
@@ -315,7 +266,7 @@ Ext.setup({
             if(!game.initialized) {
               initializeGame(loc);
             } else {
-             game.you.setPosition(loc.latitude+0.0002, loc.longitude+0.0002);
+             game.you.setPosition(loc.latitude, loc.longitude);
              console.log(game.you.data)
             }
             
